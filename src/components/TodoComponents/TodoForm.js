4b7bb493c7a0cfba11624b243a43f8392f814308
Sam <Jeffcoat>
2019-10-28 15:14:07 -0500

    TodoForm built

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,1 +1,28 @@
-import 
+import React from 'react';
+
+class TodoForm extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            newTodo: ' '
+        }
+    }
+    render(){
+        return(
+            <form>
+                <label htmlFor = "todo">New Todo</label>
+                <input
+                type= "text"
+                name = "todo"
+                id= "item"
+                value = {this.state.newTodo}
+                onChange= {this.handleChanges}
+                />
+                <button>Add</button>
+            </form>
+        )
+    }
+
+}
+
+export default TodoForm;
